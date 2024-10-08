ca4674bd2f26103dd1102a7357325aab13b9db16
yekk1 <daeyoi96@gmail.com>
2024-10-09 02:32:12 +0900

    feat: spring project 생성

---
+++ src/test/java/com/sparta/hhplusconcert/HHplusConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hhplusconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHplusConcertApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
