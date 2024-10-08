b0b6e83c1ca14c3002192e19de28269b38f97b06
yekk1 <daeyoi96@gmail.com>
2024-10-09 02:32:12 +0900

    feat: spring project 생성
    (cherry picked from commit ca4674bd2f26103dd1102a7357325aab13b9db16)

---
+++ src/main/java/com/sparta/hhplusconcert/HHplusConcertApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hhplusconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHplusConcertApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HHplusConcertApplication.class, args);
+  }
+
+}
