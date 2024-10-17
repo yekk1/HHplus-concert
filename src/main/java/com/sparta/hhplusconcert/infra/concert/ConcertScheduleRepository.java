d0d3d027e1e93b421fdf6de3e47cfd614f1cb1a8
yekk1 <daeyoi96@gmail.com>
2024-10-17 22:45:31 +0900

    feat: 예약 가능 날짜 조회 비즈니스 로직 구현

--- src/main/java/com/sparta/hhplusconcert/infra/concert/ConcertScheduleRepository.java
+++ src/main/java/com/sparta/hhplusconcert/infra/concert/ConcertScheduleRepository.java
@@ -3,7 +3,7 @@ package com.sparta.hhplusconcert.infra.concert;
 import com.sparta.hhplusconcert.domain.concert.entity.ConcertScheduleEntity;
 import java.util.List;
 
-public interface ConcertSchedulRepository {
+public interface ConcertScheduleRepository {
   List<ConcertScheduleEntity> getScheduleByConcertId(Long concertId);
 
 }
